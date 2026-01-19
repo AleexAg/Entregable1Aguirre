30d612c403c4f39173f446e51e03753ff4c31f3d
Alex <aguirre.alex.na@gmail.com>
2026-01-19 13:20:10 -0300

    DOM

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let claveAcceso = "1867";
+let claveAcceso = "123";
 let precioSalida = 330800;
 
 let intentos = 3;
@@ -7,110 +7,85 @@ let dineroDisponible = 35000;
 
 let catalogoJuegos = ["Elden Ring","God of War Ragnarök - Aventura", "FIFA 26", "GTA VI"];
 
-function validarPIN() {
-    alert("Bienvenido al sistema de reserva");
 
-    for (let i = 1; i <= intentos; i++) {
-        let pinIngresado = prompt("Ingrese su PIN:");
+const pinInput = document.getElementById('pinInput')
+const btnIngresar = document.getElementById('btnIngresar')
+const login = document.getElementById("login");
+const ingresarPin = document.getElementById('ingresarPin')
+const menu = document.getElementById("menu");
+const deposito = document.getElementById("deposito");
+const resultado = document.getElementById("resultado");
+const btnPrecio = document.getElementById("btnPrecio");
+const btnComprar = document.getElementById("btnComprar");
+const btnDeposito = document.getElementById("btnDeposito");
+const montoDeposito = document.getElementById("montoDeposito");
+const btnConfirmarDeposito = document.getElementById("btnConfirmarDeposito");
+const btnCatalogo = document.getElementById("btnCatalogo");
 
-        if (pinIngresado === claveAcceso) {
-            alert("PIN correcto. Bienvenido");
-            return true;
-        } else {
-            alert("PIN incorrecto. Intento " + i + " de " + intentos);
+
+btnIngresar.addEventListener('click', () => {
+    if(pinInput.value == claveAcceso){
+        resultado.innerHTML = 'Pin correcto';
+        login.style.display = 'none';
+        menu.style.display = 'block';
+    } else {
+        intentos--;
+        resultado.innerHTML = 'Pin incorrecto, intentelo nuevamente. Intentos restantes:' + intentos;
+
+        if (intentos == 0) {
+            ingresarPin.innerHTML = 'Acceso bloqueado';
+            pinInput.style.display = 'none';
+            resultado.style.display = 'none';
+            btnIngresar.style.display = 'none';
         }
     }
-
-    return false;   
-}
-
-
-function consultarPrecioEntrada() {
-    alert("El precio de salida es: $" + precioSalida + "\nTu saldo disponible es de: $" + dineroDisponible);
-    console.log("Precio y saldo consultado:", precioSalida, dineroDisponible);
-}
+})
 
 
 
-function realizarCompra() {
-    alert("El precio de reserva del juego es $" + precioSalida);
+btnPrecio.addEventListener('click', () => {
+    resultado.innerHTML = 'Precio de salida: $' + precioSalida +
+    '<br>Saldo disponible: $' + dineroDisponible; 
 
+})
+
+
+btnComprar.addEventListener('click', () => {
     if (dineroDisponible < precioSalida) {
-        alert("No tienes suficiente dinero para realizar la compra.");
-        return;
+        resultado.innerHTML = 'No tienes saldo suficiente';
     } else {
         dineroDisponible -= precioSalida;
-        alert("Reserva realizada con exito \nNuevo saldo: $" + dineroDisponible);
-        console.log("Compra realizada. Saldo restante:", dineroDisponible);
+        resultado.innerHTML = 'Reserva realizada <br>Nuevo saldo: $' + dineroDisponible;
     }
-}
+})
 
-function realizarDeposito() {
-    let monto = parseFloat(prompt("Ingrese el monto a depositar:"));
-    
-    if (monto <= 0) {
-        alert("Monto inválido.");
-        return;
-    } else {
+
+btnDeposito.addEventListener('click', () => {
+    deposito.style.display = 'block';
+});
+
+
+btnConfirmarDeposito.addEventListener('click', () => {
+    let monto = Number(montoDeposito.value);
+
+    if (monto > 0) {
         dineroDisponible += monto;
-        alert("Depósito exitoso. Nuevo saldo: $" + dineroDisponible);
-        console.log("Depósito:", monto, "Saldo actual:", dineroDisponible);
+        resultado.innerHTML =
+            'Deposito exitoso.<br>Saldo actual: $' + dineroDisponible;
+        montoDeposito.value = '';
+        deposito.style.display = 'none';
+    } else {
+        resultado.innerHTML = 'Monto inválido';
     }
-}
+});
 
-
-function verCatalogo() {
-    let mensaje = "Catálogo de juegos disponibles:\n\n";
+btnCatalogo.addEventListener('click', () => {
+    let html = '<h4>Catalogo disponible:</h4><ul>';
 
     for (let i = 0; i < catalogoJuegos.length; i++) {
-        mensaje += catalogoJuegos[i] + "\n";
+        html += '<li>' + catalogoJuegos[i] + '</li>';
     }
 
-    alert(mensaje);
-    console.log("Catálogo mostrado:", catalogoJuegos);
-}
-
-function iniciarSistema() {
-    if (validarPIN()) {
-        let opcion = prompt(
-            "Seleccione una opción:\n" +
-            "1) Consultar precio de salida y saldo en cuenta\n" +
-            "2) Realizar compra\n" +
-            "3) Realizar deposito de dinero\n" +
-            "4) Ver catalogo de juegos disponibles\n" +
-            "5) Salir"
-        );
-
-        while (opcion != "5"){
-            if (opcion == "1") {
-                consultarPrecioEntrada();
-            } 
-            else if (opcion === "2") {
-                realizarCompra();
-            } 
-            else if (opcion === "3") {
-                realizarDeposito();
-            } 
-            else if (opcion === "4") {
-                verCatalogo(); 
-            } 
-            else {
-                alert("Opción inválida, intenta nuevamente.");
-            }
-            
-            opcion = prompt(
-                "Seleccione una opción:\n" +
-                "1) Consultar precio de salida y saldo en cuenta\n" +
-                "2) Realizar compra\n" +
-                "3) Realizar deposito de dinero\n" +
-                "4) Ver catalogo de juegos disponibles\n" +
-                "5) Salir"
-            );
-        }
-    } else{
-        alert("Has superado los intentos permitidos. Comuníquese con el encargado del sistema.")
-
-    }
-}
-
-iniciarSistema()
+    html += '</ul>';
+    resultado.innerHTML = html;
+});
